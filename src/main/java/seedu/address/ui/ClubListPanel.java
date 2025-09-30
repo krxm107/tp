684374b008fdebb58add26a2ff2b419c051741c2
Matthew Kan (NUS Student) <e0795274@u.nus.edu>
2025-09-30 14:05:02 +0800

    Add Club List Pane to UI

---
+++ src/main/java/seedu/address/ui/ClubListPanel.java
@@ -0,0 +1,49 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.person.Person;
+
+/**
+ * Panel containing the list of clubs.
+ */
+public class ClubListPanel extends UiPart<Region> {
+    private static final String FXML = "ClubListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(ClubListPanel.class);
+
+    @FXML
+    private ListView<Person> personListView;
+
+    /**
+     * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
+     */
+    public ClubListPanel(ObservableList<Person> personList) {
+        super(FXML);
+        personListView.setItems(personList);
+        personListView.setCellFactory(listView -> new PersonListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
+     */
+    class PersonListViewCell extends ListCell<Person> {
+        @Override
+        protected void updateItem(Person person, boolean empty) {
+            super.updateItem(person, empty);
+
+            if (empty || person == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+}
