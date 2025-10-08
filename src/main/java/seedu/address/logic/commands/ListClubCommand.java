300cfb41a75cd8fec3450e57a031f938e2ffbd0c
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-10-08 19:17:52 +0800

    Fix style errors

--- src/main/java/seedu/address/logic/commands/ListClubCommand.java
+++ src/main/java/seedu/address/logic/commands/ListClubCommand.java
@@ -4,7 +4,6 @@ import seedu.address.model.Model;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_CLUBS;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 /**
  * Lists all persons in the address book to the user.
