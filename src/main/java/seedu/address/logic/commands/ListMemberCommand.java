3c2ea2087a7a13a171ec2bd90b5ffa70c86f42a6
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-10-08 19:39:51 +0800

    Add ListMemberCommand class

---
+++ src/main/java/seedu/address/logic/commands/ListMemberCommand.java
@@ -0,0 +1,73 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.club.Club;
+
+import java.util.List;
+
+/**
+ * Lists all persons in the address book to the user.
+ */
+public class ListMemberCommand extends Command {
+
+    public static final String COMMAND_WORD = "members";
+
+    public static final String MESSAGE_SUCCESS = "Listed all clubs";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": List all members of a club indicated by its index in the club list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_LIST_SUCCESS = "Listed all members";
+
+    private final Index targetIndex;
+
+    public ListMemberCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Club> lastShownList = model.getFilteredClubList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            //throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            throw new CommandException("Bad index");
+        }
+
+        Club clubToDisplay = lastShownList.get(targetIndex.getZeroBased());
+        model.updateFilteredClubList(club -> club.equals(clubToDisplay));
+        model.updateFilteredPersonList(person -> true);
+        //return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(personToDelete)));
+        return new CommandResult("Success");
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ListMemberCommand)) {
+            return false;
+        }
+
+        ListMemberCommand otherListMemberCommand = (ListMemberCommand) other;
+        return targetIndex.equals(otherListMemberCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetIndex)
+                .toString();
+    }
+}
