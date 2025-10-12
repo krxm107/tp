a9c7721f8b71df15646693d2c77f76b3389ef144
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-10-12 20:51:00 +0800

    Add NameParser class

---
+++ src/main/java/seedu/address/logic/parser/NameParser.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
+
+/**
+ * Parses input arguments and creates a new FindCommand object
+ */
+public class NameParser {
+
+    public static final String KEYWORD = "n";
+
+    public static Predicate<Person> parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        String[] nameKeywords = trimmedArgs.split("\\s+");
+
+        return new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords));
+    }
+
+}
