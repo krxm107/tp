655c7552b76de7ae638eab0a53719cf4dbb3b773
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-10-12 15:29:16 +0800

    Add ListMembershipCommand

---
+++ src/main/java/seedu/address/logic/commands/ListMembershipCommand.java
@@ -0,0 +1,77 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.club.Club;
+import seedu.address.model.membership.Membership;
+import seedu.address.model.person.Person;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Lists all memberships of a person identified using it's displayed index from the address book.
+ */
+public class ListMembershipCommand extends Command {
+
+    public static final String COMMAND_WORD = "list_memberships";
+
+    public static final String MESSAGE_SUCCESS = "Listed all memberships";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": List all memberships of a person identified by the index number used in the displayed person list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_LIST_SUCCESS = "Listed all memberships";
+
+    private final Index targetIndex;
+
+    public ListMembershipCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_CLUB_DISPLAYED_INDEX);
+        }
+
+        Person personToDisplay = lastShownList.get(targetIndex.getZeroBased());
+        model.updateFilteredPersonList(person -> person.equals(personToDisplay));
+        Predicate<Club> isMemberOf = club -> club.getMemberships().stream().map(Membership::getPerson)
+                .anyMatch(person -> person.equals(personToDisplay));
+        model.updateFilteredClubList(isMemberOf);
+        return new CommandResult(MESSAGE_LIST_SUCCESS);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ListMembershipCommand)) {
+            return false;
+        }
+
+        ListMembershipCommand otherListMemberhipCommand = (ListMembershipCommand) other;
+        return targetIndex.equals(otherListMemberhipCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetIndex)
+                .toString();
+    }
+}
