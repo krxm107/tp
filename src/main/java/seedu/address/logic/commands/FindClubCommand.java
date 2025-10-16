bd2028e3c548e4c5004cf2fcfb4cbad64bac2a58
yjjr0 <richard.yjunjie@gmail.com>
2025-10-17 00:19:30 +0800

    Implement FindClubCommand, FindClubCommandParser, ClubContainsKeywordsPredicate

--- src/main/java/seedu/address/logic/commands/FindClubCommand.java
+++ src/main/java/seedu/address/logic/commands/FindClubCommand.java
@@ -1,4 +1,59 @@
 package seedu.address.logic.commands;
 
-public class FindClubCommand {
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.club.ClubContainsKeywordsPredicate;
+
+/**
+ * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Keyword matching is case insensitive.
+ */
+public class FindClubCommand extends Command {
+
+    public static final String COMMAND_WORD = "find_club";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all clubs whose fields contains any of "
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: n/NAME p/PHONE_NUMBER e/EMAIL a/ADDRESS [t/TAG]…\u200B\n"
+            + "Example: " + COMMAND_WORD
+            + "n/Basketball p/98765432 e/johnd@example.com a/John street, block 123, #01-01";
+
+    private final ClubContainsKeywordsPredicate predicate;
+
+    public FindClubCommand(ClubContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredClubList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredClubList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FindClubCommand)) {
+            return false;
+        }
+
+        FindClubCommand otherFindCommand = (FindClubCommand) other;
+        return predicate.equals(otherFindCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("predicate", predicate)
+                .toString();
+    }
 }
--- src/main/java/seedu/address/logic/parser/FindClubCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindClubCommandParser.java
@@ -1,4 +1,33 @@
 package seedu.address.logic.parser;
 
-public class FindClubCommandParser {
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+
+import seedu.address.logic.commands.FindClubCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.club.ClubContainsKeywordsPredicate;
+
+/**
+ * Parses input arguments and creates a new FindCommand object
+ */
+public class FindClubCommandParser implements Parser<FindClubCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand
+     * and returns a FindCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public FindClubCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindClubCommand.MESSAGE_USAGE));
+        }
+
+        String[] nameKeywords = trimmedArgs.split(" (?=\\w/)");
+
+        return new FindClubCommand(new ClubContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+    }
+
 }
