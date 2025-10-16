5f906f9311074a96c71c0bec599ca8b7f7becb5d
yjjr0 <richard.yjunjie@gmail.com>
2025-10-16 18:40:28 +0800

    Fix checkstyle errors in CommandList

--- src/main/java/seedu/address/logic/parser/CommandList.java
+++ src/main/java/seedu/address/logic/parser/CommandList.java
@@ -3,26 +3,45 @@ package seedu.address.logic.parser;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A utility class to maintain a history of user-entered commands,
+ * allowing navigation through previous and next commands.
+ */
 public class CommandList {
-    private static final List<String> COMMANDLIST = new ArrayList<String>();
+    private static final List<String> COMMAND_LIST = new ArrayList<>();
     private static int currentCommand = 0;
 
+    /**
+     * Adds a new command to the command history.
+     *
+     * @param args The command string to add.
+     */
     public static void addCommand(String args) {
-        COMMANDLIST.add(args);
-        currentCommand = COMMANDLIST.size();
+        COMMAND_LIST.add(args);
+        currentCommand = COMMAND_LIST.size();
     }
 
+    /**
+     * Returns the previous command in the history, if available.
+     *
+     * @return The previous command string.
+     */
     public static String getPrevCommand() {
         if (currentCommand > 0) {
             currentCommand--;
         }
-        return COMMANDLIST.get(currentCommand);
+        return COMMAND_LIST.get(currentCommand);
     }
 
+    /**
+     * Returns the next command in the history, if available.
+     *
+     * @return The next command string.
+     */
     public static String getNextCommand() {
-        if (currentCommand < COMMANDLIST.size() - 1) {
+        if (currentCommand < COMMAND_LIST.size() - 1) {
             currentCommand++;
         }
-        return COMMANDLIST.get(currentCommand);
+        return COMMAND_LIST.get(currentCommand);
     }
 }
