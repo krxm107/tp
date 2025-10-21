678aef04a8f83fe609b0551839e36020acd2503a
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-10-21 16:45:49 +0800

    Implement GetClub and GetPerson Commands

--- src/main/java/seedu/address/logic/commands/GetClubCommand.java
+++ src/main/java/seedu/address/logic/commands/GetClubCommand.java
@@ -1,4 +1,74 @@
 package seedu.address.logic.commands;
 
-public class GetClubCommand {
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CopyUtil;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.club.Club;
+
+public class GetClubCommand extends Command {
+
+    public static final String COMMAND_WORD = "get_club";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Copies the club identified by the index number to the user's clipboard.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_GET_CLUB_SUCCESS = "Copied club: %1$s";
+    public static final String MESSAGE_GET_CLUB_FAILURE = "Failed to copy club to clipboard";
+
+    private final Index targetIndex;
+
+    public GetClubCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Club> lastShownList = model.getFilteredClubList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_CLUB_DISPLAYED_INDEX);
+        }
+
+        Club clubToCopy = lastShownList.get(targetIndex.getZeroBased());
+        try {
+            CopyUtil.copyTextToClipboard(clubToCopy.toString());
+        } catch (Exception e) {
+            throw new CommandException(MESSAGE_GET_CLUB_FAILURE);
+        }
+
+        return new CommandResult(String.format(MESSAGE_GET_CLUB_SUCCESS, Messages.format(clubToCopy)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof GetClubCommand)) {
+            return false;
+        }
+
+        GetClubCommand otherGetCommand = (GetClubCommand) other;
+        return targetIndex.equals(otherGetCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetIndex)
+                .toString();
+    }
+
 }
--- src/main/java/seedu/address/logic/commands/GetPersonCommand.java
+++ src/main/java/seedu/address/logic/commands/GetPersonCommand.java
@@ -1,4 +1,74 @@
 package seedu.address.logic.commands;
 
-public class GetPersonCommand {
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CopyUtil;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+public class GetPersonCommand extends Command {
+
+    public static final String COMMAND_WORD = "get_person";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Copies the person identified by the index number to the user's clipboard.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_GET_PERSON_SUCCESS = "Copied person: %1$s";
+    public static final String MESSAGE_GET_PERSON_FAILURE = "Failed to copy person to clipboard";
+
+    private final Index targetIndex;
+
+    public GetPersonCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToCopy = lastShownList.get(targetIndex.getZeroBased());
+        try {
+            CopyUtil.copyTextToClipboard(personToCopy.toString());
+        } catch (Exception e) {
+            throw new CommandException(MESSAGE_GET_PERSON_FAILURE);
+        }
+
+        return new CommandResult(String.format(MESSAGE_GET_PERSON_SUCCESS, Messages.format(personToCopy)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof GetPersonCommand)) {
+            return false;
+        }
+
+        GetPersonCommand otherGetCommand = (GetPersonCommand) other;
+        return targetIndex.equals(otherGetCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetIndex)
+                .toString();
+    }
+
 }
