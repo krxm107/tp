c7d0ca7eb06daf40c36a2e51f527c4c97fa6e7b1
Nguyen The Bach <178692316+djgmh9@users.noreply.github.com>
2025-10-22 10:45:52 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/CancelMembershipCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CancelMembershipCommandParser.java
@@ -1,15 +1,15 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CLUB;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MEMBER;
+
+import java.util.stream.Stream;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.CancelMembershipCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
-import java.util.stream.Stream;
-
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_CLUB;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DURATION;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_MEMBER;
 
 /**
  * Parses input arguments and creates a new CancelMembershipCommand object
