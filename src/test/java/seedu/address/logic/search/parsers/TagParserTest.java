1a0139958b42515551183de4f4520eaf9a5822c7
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-10-25 19:11:19 +0800

    Add tests for remaining SearchParsers

---
+++ src/test/java/seedu/address/logic/search/parsers/TagParserTest.java
@@ -0,0 +1,39 @@
+package seedu.address.logic.search.parsers;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.search.predicates.TagsMatchPredicate;
+import seedu.address.model.person.Person;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.function.Predicate;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.search.SearchParserTestUtil.assertParseFailure;
+import static seedu.address.logic.search.SearchParserTestUtil.assertParseSuccess;
+
+public class TagParserTest {
+
+    private TagParser<Person> parser = new TagParser<>();
+
+    @Test
+    public void parse_validArgs_returnsCorrectPredicate() {
+        // one keyword
+        Predicate<Person> expectedPredicate =
+                new TagsMatchPredicate<>(Collections.singletonList("friend"));
+
+        assertParseSuccess(parser, " friend ", expectedPredicate);
+
+        // multiple keywords
+        expectedPredicate = new TagsMatchPredicate<>(Arrays.asList("friend", "nus"));
+        assertParseSuccess(parser, " \n friend \n \t nus  \t", expectedPredicate);
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        // empty argument
+        assertParseFailure(parser, "   ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SearchParser.MESSAGE_USAGE));
+    }
+
+}
