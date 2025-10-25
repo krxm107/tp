7c1cb82fe9fe462f8d5623dda7196ac1a98924f1
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-10-25 22:32:49 +0800

    Add GetPersonCommandTest

---
+++ src/test/java/seedu/address/logic/commands/GetPersonCommandTest.java
@@ -0,0 +1,110 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.logic.parser.get.GetPersonMessageParser;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static seedu.address.logic.commands.CommandTestUtil.*;
+import static seedu.address.testutil.TypicalIndexes.*;
+import static seedu.address.testutil.TypicalPersonsAndClubs.getTypicalAddressBook;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code GetPersonCommand}.
+ */
+public class GetPersonCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_validIndexUnfilteredList_success() {
+        Person personToGet = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        GetPersonCommand getPersonCommand = new GetPersonCommand(INDEX_FIRST_PERSON, "");
+
+        String copiedText = new GetPersonMessageParser().parse(personToGet, "");
+        String expectedMessage = String.format(GetPersonCommand.MESSAGE_GET_PERSON_SUCCESS, copiedText);
+
+        assertCommandSuccess(getPersonCommand, model, expectedMessage, model);
+    }
+
+    @Test
+    public void execute_invalidIndexUnfilteredList_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        GetPersonCommand getPersonCommand = new GetPersonCommand(outOfBoundIndex, "");
+
+        assertCommandFailure(getPersonCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_validIndexFilteredList_success() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        Person personToGet = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        GetPersonCommand getPersonCommand = new GetPersonCommand(INDEX_FIRST_PERSON, "");
+
+        String copiedText = new GetPersonMessageParser().parse(personToGet, "");
+        String expectedMessage = String.format(GetPersonCommand.MESSAGE_GET_PERSON_SUCCESS, copiedText);
+
+        assertCommandSuccess(getPersonCommand, model, expectedMessage, model);
+    }
+
+    @Test
+    public void execute_invalidIndexFilteredList_throwsCommandException() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+
+        Index outOfBoundIndex = INDEX_SECOND_PERSON;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+
+        GetPersonCommand getPersonCommand = new GetPersonCommand(outOfBoundIndex, "");
+
+        assertCommandFailure(getPersonCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_getSpecifiedFields_success() {
+        Person personToGet = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        GetPersonCommand getPersonCommand = new GetPersonCommand(INDEX_FIRST_PERSON, "/aenpt");
+
+        String copiedText = new GetPersonMessageParser().parse(personToGet, "/aenpt");
+        String expectedMessage = String.format(GetPersonCommand.MESSAGE_GET_PERSON_SUCCESS, copiedText);
+
+        assertCommandSuccess(getPersonCommand, model, expectedMessage, model);
+    }
+
+    @Test
+    public void equals() {
+        GetPersonCommand getPersonFirstCommand = new GetPersonCommand(INDEX_FIRST_PERSON, "");
+        GetPersonCommand getPersonSecondCommand = new GetPersonCommand(INDEX_SECOND_PERSON, "");
+
+        // same object -> returns true
+        assertTrue(getPersonFirstCommand.equals(getPersonFirstCommand));
+
+        // same values -> returns true
+        GetPersonCommand getPersonFirstCommandCopy = new GetPersonCommand(INDEX_FIRST_PERSON, "");
+        assertTrue(getPersonFirstCommand.equals(getPersonFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(getPersonFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(getPersonFirstCommand.equals(null));
+
+        // different club -> returns false
+        assertFalse(getPersonFirstCommand.equals(getPersonSecondCommand));
+    }
+
+    @Test
+    public void toStringMethod() {
+        Index targetIndex = Index.fromOneBased(1);
+        GetPersonCommand getPersonCommand = new GetPersonCommand(targetIndex, "");
+        String expected = GetPersonCommand.class.getCanonicalName() + "{targetIndex=" + targetIndex + "}";
+        assertEquals(expected, getPersonCommand.toString());
+    }
+
+}
