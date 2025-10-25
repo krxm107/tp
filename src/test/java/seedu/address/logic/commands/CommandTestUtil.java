c8021e059f847152e2ec896a5685f94c9012d1bb
Matthew Kan (NUS Student) <e0795274@u.nus.edu>
2025-10-25 17:43:39 +0800

    Fix all style errors

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -78,9 +78,7 @@ public class CommandTestUtil {
     public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James!"; // '!' not allowed by Name regex
     public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
     public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
-    public static final String INVALID_ADDRESS_DESC = "" +
-            "" +
-            " " + PREFIX_ADDRESS + "Blk 123 ! Road"; // '!' not allowed
+    public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS + "Blk 123 ! Road"; // '!' not allowed
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
 
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
