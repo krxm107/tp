e6473aeac53f4abede4d1c2fd48c6b14c904cb4a
Nguyen The Bach <178692316+djgmh9@users.noreply.github.com>
2025-10-25 10:31:01 +0800

    Auto scroll to last added person

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -2,6 +2,8 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
+import javafx.application.Platform;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
@@ -27,7 +29,30 @@ public class PersonListPanel extends UiPart<Region> {
         super(FXML);
         personListView.setItems(personList);
         personListView.setCellFactory(listView -> new PersonListViewCell());
+
+        // Add a listener to scroll to the newly added person.
+        personList.addListener((ListChangeListener<Person>) c -> {
+            while (c.next()) {
+                // We are only interested in additions to the list.
+                if (c.wasAdded()) {
+                    // Get the person that was just added.
+                    // c.getAddedSubList() returns a list of all items added.
+                    // We'll scroll to the first one in the list of additions.
+                    if (!c.getAddedSubList().isEmpty()) {
+                        Person addedPerson = c.getAddedSubList().get(0);
+
+                        // Use Platform.runLater to ensure this happens after the UI pass.
+                        Platform.runLater(() -> {
+                            // Scroll to the specific Person object. This is robust and
+                            // works correctly even if the list is sorted.
+                            personListView.scrollTo(addedPerson);
+                        });
+                    }
+                }
+            }
+        });
     }
+
 
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
