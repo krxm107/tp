1716c68687271dadf6e4bab9eca9003ac930dd66
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-10-26 16:17:50 +0800

    Modify ListMember/MembershipCommandParsers

--- src/main/java/seedu/address/logic/parser/ListMemberCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListMemberCommandParser.java
@@ -3,6 +3,7 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.GetClubCommand;
 import seedu.address.logic.commands.ListMemberCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -17,12 +18,20 @@ public class ListMemberCommandParser implements Parser<ListMemberCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public ListMemberCommand parse(String args) throws ParseException {
+        String[] parts = args.split("/");
+        Index index;
+
         try {
-            Index index = ParserUtil.parseIndex(args);
-            return new ListMemberCommand(index);
+            index = ParserUtil.parseIndex(parts[0]);
         } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListMemberCommand.MESSAGE_USAGE), pe);
+        }
+
+        if (parts.length == 1) {
+            return new ListMemberCommand(index);
+        } else {
+            return new ListMemberCommand(index);
         }
     }
 
--- src/main/java/seedu/address/logic/parser/ListMembershipCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListMembershipCommandParser.java
@@ -3,6 +3,7 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.ListMemberCommand;
 import seedu.address.logic.commands.ListMembershipCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -17,12 +18,20 @@ public class ListMembershipCommandParser implements Parser<ListMembershipCommand
      * @throws ParseException if the user input does not conform the expected format
      */
     public ListMembershipCommand parse(String args) throws ParseException {
+        String[] parts = args.split("/");
+        Index index;
+
         try {
-            Index index = ParserUtil.parseIndex(args);
-            return new ListMembershipCommand(index);
+            index = ParserUtil.parseIndex(args);
         } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListMembershipCommand.MESSAGE_USAGE), pe);
+        }
+
+        if (parts.length == 1) {
+            return new ListMembershipCommand(index);
+        } else {
+            return new ListMembershipCommand(index);
         }
     }
 
