6aaa138d9d8b4d9ed7ecd3c9df945c3bdb770c63
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-10-26 18:26:33 +0800

    Update ListMember/MembershipCommandParserTests

--- src/test/java/seedu/address/logic/parser/ListMemberCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ListMemberCommandParserTest.java
@@ -9,26 +9,38 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.ListMemberCommand;
 
-/**
- * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the ListMemberCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the ListMemberCommand, and therefore we test only one of them.
- * The path variation for those two cases occur inside the ParserUtil, and
- * therefore should be covered by the ParserUtilTest.
- */
 public class ListMemberCommandParserTest {
 
     private ListMemberCommandParser parser = new ListMemberCommandParser();
 
     @Test
     public void parse_validArgs_returnsListCommand() {
-        assertParseSuccess(parser, "1",
-                new ListMemberCommand(INDEX_FIRST_CLUB, membership -> true));
+        // No keywords
+        assertParseSuccess(parser, "1  ",
+                new ListMemberCommand(INDEX_FIRST_CLUB, new MembershipStatusPredicate()));
+
+        // Empty keywords
+        assertParseSuccess(parser, "1 /  ",
+                new ListMemberCommand(INDEX_FIRST_CLUB, new MembershipStatusPredicate()));
+
+        // Valid keywords
+        MembershipStatusPredicate predicate = new MembershipStatusPredicate();
+        predicate.addPredicate("a");
+        predicate.addPredicate("c");
+        assertParseSuccess(parser, "1 /ac", new ListMemberCommand(INDEX_FIRST_CLUB, predicate));
+
+        // Valid keywords with random letters
+        assertParseSuccess(parser, "1 / abcd  ", new ListMemberCommand(INDEX_FIRST_CLUB, predicate));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+        // Missing index
+        assertParseFailure(parser, "/ac", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                ListMemberCommand.MESSAGE_USAGE));
+
+        // Invalid index
+        assertParseFailure(parser, "-1", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 ListMemberCommand.MESSAGE_USAGE));
     }
 }
