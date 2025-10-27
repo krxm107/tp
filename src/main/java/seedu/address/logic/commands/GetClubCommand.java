2e2ad8feeb7dd5b7d87c76c6151e6ec5abe6f994
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-10-27 22:16:27 +0800

    Update get command message usage

--- src/main/java/seedu/address/logic/commands/GetClubCommand.java
+++ src/main/java/seedu/address/logic/commands/GetClubCommand.java
@@ -23,7 +23,7 @@ public class GetClubCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + " (" + COMMAND_SHORT
             + "): Copies details of a club identified by the index number to the user's clipboard.\n"
-            + "Parameters: INDEX (must be a positive integer) /[OPTIONAL KEYWORDS]\n"
+            + "Parameters: INDEX (must be a positive integer) [OPTIONAL KEYWORDS]\n"
             + "Optional keywords may be added to copy specified fields only.\n"
             + "Keywords: n - name, p - phone, e - email, a - address, m - members, * - full details\n"
             + "The '*' keyword specifies all club details to be copied alongside all member details.\n"
