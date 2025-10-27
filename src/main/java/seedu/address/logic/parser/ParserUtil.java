6c46acf1b26ef70588f5b4700259d993c7429184
Matthew Kan (NUS Student) <e0795274@u.nus.edu>
2025-10-27 23:13:30 +0800

    Make whitespaces in ParserUtil's parseEmail to improve readability

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -132,9 +132,11 @@ public class ParserUtil {
     public static Email parseEmail(String email) throws ParseException {
         requireNonNull(email);
         String strippedEmail = email.strip();
+
         if (!Email.isValidEmail(strippedEmail)) {
             throw new ParseException(Email.MESSAGE_CONSTRAINTS);
         }
+
         return new Email(strippedEmail);
     }
 
