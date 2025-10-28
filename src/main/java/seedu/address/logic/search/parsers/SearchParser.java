caa28fc85f318673f41ea38aae2154d688f4dd40
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-10-29 00:50:34 +0800

    Update command usage message

--- src/main/java/seedu/address/logic/search/parsers/SearchParser.java
+++ src/main/java/seedu/address/logic/search/parsers/SearchParser.java
@@ -10,8 +10,8 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public interface SearchParser<T> {
 
-    String MESSAGE_USAGE = "Parameters: [/SEARCH_CONDITIONS SEARCH_PARAMETERS]...\n"
-            + "For each condition, one or more search parameters must be provided,"
+    String MESSAGE_USAGE = "Parameters: [/SEARCH_CONDITIONS SEARCH_KEYWORDS]...\n"
+            + "For each condition, one or more search keywords must be provided,"
             + " any of which may be used to match the target.\n";
 
     /**
