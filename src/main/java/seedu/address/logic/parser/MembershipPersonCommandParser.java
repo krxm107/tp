d980abb4792ba9dad4c05b22d453418e272354f9
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-11-01 14:40:13 +0800

    Fix style issues

--- src/main/java/seedu/address/logic/parser/MembershipPersonCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MembershipPersonCommandParser.java
@@ -3,7 +3,6 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.MembershipClubCommand;
 import seedu.address.logic.commands.MembershipPersonCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.logic.search.predicates.MembershipStatusPredicate;
--- src/main/java/seedu/address/model/membership/MembershipStatus.java
+++ src/main/java/seedu/address/model/membership/MembershipStatus.java
@@ -3,7 +3,6 @@ package seedu.address.model.membership;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Represents the status of a Membership.
