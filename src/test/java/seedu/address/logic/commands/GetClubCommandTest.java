9efa72085a0f033195dde34fa783b546b6919621
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-11-01 13:47:05 +0800

    Remove unused imports

--- src/test/java/seedu/address/logic/commands/GetClubCommandTest.java
+++ src/test/java/seedu/address/logic/commands/GetClubCommandTest.java
@@ -1,25 +1,5 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showClubAtIndex;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CLUB;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_CLUB;
-import static seedu.address.testutil.TypicalPersonsAndClubs.getTypicalAddressBook;
-
-import org.junit.jupiter.api.Test;
-
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.Messages;
-import seedu.address.logic.parser.GetClubMessageParser;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
-import seedu.address.model.club.Club;
-
 /**
  * Contains integration tests (interaction with the Model) for {@code GetClubCommand}.
  */
